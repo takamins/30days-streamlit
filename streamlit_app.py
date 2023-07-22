b16e8d882d78f5cfda4973a0feed94b57c9aba4f
takamins <dwp397@gmail.com>
2023-07-22 09:48:21 +0900

    day8

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,39 @@
 import streamlit as st
+from datetime import time, datetime
 
-st.write('Hello world!')
+st.header('st.slider')
 
-st.header('st.button')
+#例1
 
-if st.button('Say hello'):
-    st.write('Why hello there')
-else:
-    st.write('Goodbye')
+st.subheader('Slider')
+
+age = st.slider('How old are you?', 0, 130, 25)
+st.write("I'm ", age, 'years old')
+
+#例2
+
+st.subheader('Range slider')
+
+values = st.slider(
+     'Select a range of values',
+     0.0, 100.0, (25.0, 75.0))
+st.write('Values:', values)
+
+#例3
+
+st.subheader('Range time slider')
+
+appointment = st.slider(
+     "Schedule your appointment:",
+     value=(time(11, 30), time(12, 45)))
+st.write("You're scheduled for:", appointment)
+
+#例4
+
+st.subheader('Datetime slider')
+
+start_time = st.slider(
+     "When do you start?",
+     value=datetime(2020, 1, 1, 9, 30),
+     format="MM/DD/YY - hh:mm")
+st.write("Start time:", start_time)
