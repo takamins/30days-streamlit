99dcdc06ea117d03e23a9d8aed3aa739d87e1ccf
takamins <dwp397@gmail.com>
2023-07-22 09:31:46 +0900

    day5

---
+++ day5.py
@@ -0,0 +1,35 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+st.header('st.write')
+
+#例1
+
+st.write('Hello, *World!* emoji')
+
+#例2
+
+st.write(1234)
+
+#例3
+
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+st.write(df)
+
+#例4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+#例5
+
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+    x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
